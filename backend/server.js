3620bd8e3076a1352044af53a599dbb3a22aef3f
Nieswieze <lamehunter@gmail.com>
2022-10-09 17:34:14 +0200

    goals controller modified

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5001
+const { errorHandler } = require('./middleware/errorMiddleware')
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 //this will move /api/goals to file in routes folder
 app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.listen(port, () => console.log(`server started on port ${port}`))
 
+app.use(errorHandler)
